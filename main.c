7d9175303b0c5253c35756a730e3691700944c56
siva <siva.123.ss31@gmail.com>
2025-03-10 12:35:46 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include "apc.h"
+
+Status check_if_number(char *argv)
+{
+    for(int i=0 ; argv[i]!='\0'; i++)
+    {
+        if(!isdigit(argv[i])) return FAILURE;
+    }
+    return SUCCESS;
+}
+
+
+int main(int argc,char **argv)
+{
+    if(argc != 4)
+    {
+        fprintf(stderr,"please enter 4 arguments \n");
+        return FAILURE;
+    }
+    else if((argv[2][0]=='x'|| argv[2][0]=='+'||argv[2][0]=='-'||argv[2][0]=='/') && strlen(argv[2])==1 )
+    {
+        if(check_if_number(argv[1])== SUCCESS && check_if_number(argv[3]) == SUCCESS)
+        {
+            printf("Number verified\n");
+        } 
+        else
+        {
+            fprintf(stderr,"Enter a valid number\n");
+            
+        }
+    }
+    else
+    {
+        fprintf(stderr,"choose a valid operand \n");
+        return FAILURE;
+    }
+    
+    // head and tail intitialsed to NULL 
+    Dlist *head1,*tail1,*head2,*tail2,*headR,*tailR; 
+    head1 = tail1 = head2 = tail2 = tailR = headR = NULL;
+    
+    if(create_list(&head1,&tail1,argv[1])==FAILURE)
+    {
+        printf("list not created \n");
+        return FAILURE ;
+    }
+    if(create_list(&head2,&tail2,argv[3])==FAILURE)
+    {
+        printf("list not created \n");
+        return FAILURE;
+    }
+    print_list(head1);
+    print_list(head2);
+
+    switch(argv[2][0])
+    {
+        case '+':
+                printf("add\n");
+            
+                break;
+
+        default : 
+            break;
+    }
+
+}   
